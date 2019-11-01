08400f96e648c4134e0b826508fce55d594a57e3
rodolfo.lacerda <rodolfo.lacerda@memt.com.br>
2019-11-01 13:03:10 -0300

    teste

--- tests/login/login.test.js
+++ tests/login/login.test.js
@@ -6,12 +6,7 @@ module.exports = {
         browser
             .resizeWindow(1920, 1080)
             .url('https://172.16.9.10/')
-            .waitForElementVisible('.card-login', 3000)
-            .setValue('input[name=email]', 'zumbi@dospalmares.com.br')
-            .setValue('input[name=password]', 'pwd123')
-            .click('.login-button')
-            .waitForElementVisible(userInfo, 3000)
-            .assert.containsText(userInfo, 'Quilombo')
+            
             .end();
     }
 }
